bf5026b02568af1597ca08940a82bba930d0423f
andrewhamilton <andrew.hamilton@exelisinc.com>
2016-01-24 13:28:05 -0500

    major improvements to friends pages

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,3 +1,5 @@
+require 'user'
+
 class FriendsController < ApplicationController
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
 
@@ -14,20 +16,31 @@ class FriendsController < ApplicationController
 
   def new
     @friend = Friend.new
+    users = User.all.map { |user| [user.email, user.id] }
+    @params = {all_users: users, user_friends: []}
     respond_with(@friend)
   end
 
   def edit
+    users = User.all.map { |user| [user.email, user.id] }
+    user_friends = @friend.users.map { |user_friend| user_friend.id }
+    @params = {all_users: users, user_friends: user_friends}
   end
 
   def create
     @friend = Friend.new(friend_params)
     @friend.save
+    user_ids = params[:friend][:users].reject {|user_id| user_id.empty?}
+    users = user_ids.map {|user_id| User.find(user_id)}
+    @friend.users = users
     respond_with(@friend)
   end
 
   def update
-    @friend.update(email_params)
+    @friend.update(friend_params)
+    user_ids = params[:friend][:users].reject {|user_id| user_id.empty?}
+    users = user_ids.map {|user_id| User.find(user_id)}
+    @friend.users = users
     respond_with(@friend)
   end
 
