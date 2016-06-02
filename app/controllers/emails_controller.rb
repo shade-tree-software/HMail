5ee316ac3218e9d07a1826120aefd7d6164f7bfe
andrewhamilton <andrew.hamilton@exelisinc.com>
2016-06-02 09:38:38 -0400

    moved PopJob perform method into EmailsController

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -116,10 +116,75 @@ class EmailsController < ApplicationController
     render :action => :show
   end
 
+  def perform(user_id, count = 5)
+    user = User.find(user_id)
+    if user.email.end_with? '@gmail.com'
+      Email.delete_old_unapproved(user)
+      user_name = user.email
+      puts "Performing PopJob on user_id(#{user_id}), requesting #{count} #{'email'.pluralize(count)}."
+      password = user.email_pw
+      Mail.defaults do
+        retriever_method :pop3,
+                         {:address => "pop.gmail.com",
+                          :port => 995,
+                          :user_name => user_name,
+                          :password => password,
+                          :enable_ssl => true}
+      end
+
+      # get mail messages from pop server
+      mails = Mail.last(:count => count)
+      #mails = Mail.all
+
+      # insert messages into database only if they are unique (sometimes we get duplicates
+      # from the pop server)
+      if mails
+        mails = [mails] unless mails.is_a? Array
+        mails.each do |mail|
+          begin
+            subject = mail.subject || '(no subject)'
+            recipients = ((mail.to || []) + (mail.cc || [])).join(', ')
+            sender = mail.from.first
+            if ENV['ENCRYPT_EMAIL_DATA'] == 'true'
+              subject = Email.encrypt_subject(subject)
+              recipients = Email.encrypt_recipients(recipients)
+              sender = Email.encrypt_sender(sender)
+            end
+            Email.find_or_create_by(
+              user_id: user.id,
+              encrypted_subject: subject,
+              encrypted_recipients: recipients,
+              encrypted_sender: sender,
+              date: mail.date.to_i) do |new_email|
+              new_email.body = mail.to_s
+              new_email.archived = false
+              new_email.sent = (mail.from.first.end_with?('@gmail.com') && (mail.from.first.delete('.') == user_name.delete('.')))
+              new_email.unread = true
+              new_email.deleted = false
+            end
+          rescue ActiveRecord::RecordNotUnique
+            # find_or_create_by is not atomic.  If we get a RecordNotUnique exception it
+            # means another process tried to create the same record at the same time.
+            # Just try again, and this time it should find the matching record created
+            # by the other process and should not try to create the duplicate.
+            retry
+          rescue StandardError => e
+            puts 'Failed to store retrieved email.  ' + e.message
+          rescue Exception => e
+            puts 'Failed to store retrieved email.  ' + e.message
+            raise e
+          end
+        end
+      end
+    else
+      puts "PopJob is ignoring user_id(#{user_id}) because it is not a gmail account"
+    end
+  end
+
   def refresh
-    PopJob.perform(current_user.id)
+    perform(current_user.id)
     current_user.secondary_users.each do |secondary_user|
-      PopJob.perform(secondary_user.id, 1)
+      perform(secondary_user.id, 1)
     end
     render nothing: true
   end
@@ -127,7 +192,7 @@ class EmailsController < ApplicationController
   def refresh_all
     User.all.each do |user|
       unless user.email == 'none@nowhere.com' || user.email == 'guest@nowhere.none'
-        PopJob.perform(user.id, 1)
+        perform(user.id, 1)
       end
     end
     render nothing: true
