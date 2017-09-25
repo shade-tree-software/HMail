5d22ffc7321ba72171be4247342008eaf591056b
andrewhamilton <andrew.hamilton@harris.com>
2017-09-25 15:24:59 -0400

    added exception handling for gmail authentication errors

--- app/jobs/pop_job.rb
+++ app/jobs/pop_job.rb
@@ -19,50 +19,57 @@ class PopJob < ActiveJob::Base
                           :enable_ssl => true}
       end
 
-      # get mail messages from pop server
-      mails = Mail.last(:count => count)
-      #mails = Mail.all
-
-      # insert messages into database only if they are unique (sometimes we get duplicates
-      # from the pop server)
-      if mails
-        mails = [mails] unless mails.is_a? Array
-        mails.each do |mail|
-          begin
-            subject = mail.subject || '(no subject)'
-            recipients = ((mail.to || []) + (mail.cc || [])).join(', ')
-            sender = mail.from.first
-            if ENV['ENCRYPT_EMAIL_DATA'] == 'true'
-              subject = Email.encrypt_subject(subject)
-              recipients = Email.encrypt_recipients(recipients)
-              sender = Email.encrypt_sender(sender)
+      begin
+        # get mail messages from pop server
+        mails = Mail.last(:count => count)
+        #mails = Mail.all
+        
+        # insert messages into database only if they are unique (sometimes we get duplicates
+        # from the pop server)
+        if mails
+          mails = [mails] unless mails.is_a? Array
+          mails.each do |mail|
+            begin
+              subject = mail.subject || '(no subject)'
+              recipients = ((mail.to || []) + (mail.cc || [])).join(', ')
+              sender = mail.from.first
+              if ENV['ENCRYPT_EMAIL_DATA'] == 'true'
+                subject = Email.encrypt_subject(subject)
+                recipients = Email.encrypt_recipients(recipients)
+                sender = Email.encrypt_sender(sender)
+              end
+              Email.find_or_create_by(
+                  user_id: user.id,
+                  encrypted_subject: subject,
+                  encrypted_recipients: recipients,
+                  encrypted_sender: sender,
+                  date: mail.date.to_i) do |new_email|
+                new_email.body = mail.to_s
+                new_email.archived = false
+                new_email.sent = (mail.from.first.end_with?('@gmail.com') && (mail.from.first.delete('.') == user_name.delete('.')))
+                new_email.unread = true
+                new_email.deleted = false
+              end
+            rescue ActiveRecord::RecordNotUnique
+              # find_or_create_by is not atomic.  If we get a RecordNotUnique exception it
+              # means another process tried to create the same record at the same time.
+              # Just try again, and this time it should find the matching record created
+              # by the other process and should not try to create the duplicate.
+              retry
+            rescue StandardError => e
+              puts 'Failed to store retrieved email.  ' + e.message
+            rescue Exception => e
+              puts 'Failed to store retrieved email.  ' + e.message
+              raise e
             end
-            Email.find_or_create_by(
-                user_id: user.id,
-                encrypted_subject: subject,
-                encrypted_recipients: recipients,
-                encrypted_sender: sender,
-                date: mail.date.to_i) do |new_email|
-              new_email.body = mail.to_s
-              new_email.archived = false
-              new_email.sent = (mail.from.first.end_with?('@gmail.com') && (mail.from.first.delete('.') == user_name.delete('.')))
-              new_email.unread = true
-              new_email.deleted = false
-            end
-          rescue ActiveRecord::RecordNotUnique
-            # find_or_create_by is not atomic.  If we get a RecordNotUnique exception it
-            # means another process tried to create the same record at the same time.
-            # Just try again, and this time it should find the matching record created
-            # by the other process and should not try to create the duplicate.
-            retry
-          rescue StandardError => e
-            puts 'Failed to store retrieved email.  ' + e.message
-          rescue Exception => e
-            puts 'Failed to store retrieved email.  ' + e.message
-            raise e
           end
         end
+      rescue Net::POPAuthenticationError => e
+        puts 'Authentication Error.  Unable to POP from GMail.  ' + e.message
+      rescue Exception => e
+        puts 'Unable to POP from GMail.  ' + e.message
       end
+
     else
       puts "PopJob is ignoring user_id(#{user_id}) because it is not a gmail account"
     end
