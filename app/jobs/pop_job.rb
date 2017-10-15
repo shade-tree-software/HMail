786a00b3b652cbc9279dc8c7498aea5b87ef751f
Andrew Hamilton <andrew.hamilton@harris.com>
2017-10-15 19:25:33 -0400

    switching to raw sql for update to avoid weird validation error

--- app/jobs/pop_job.rb
+++ app/jobs/pop_job.rb
@@ -66,10 +66,10 @@ class PopJob < ActiveJob::Base
         end
       rescue Net::POPAuthenticationError => e
         puts "Authentication Error for user_id(#{user_id}).  Unable to POP from GMail.  #{e.message}"
-        user.update!(pop_error: true)
+        ActiveRecord::Base.connection.execute('update users set pop_error=true where id=' + user.id)
       rescue Exception => e
         puts "Exception caught for user_id(#{user_id}).  Unable to POP from GMail.  #{e.message}"
-        user.update!(pop_error: true)
+        ActiveRecord::Base.connection.execute('update users set pop_error=true where id=' + user.id)
       end
 
     else
